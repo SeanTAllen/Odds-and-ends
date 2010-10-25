4b4ce9609fb76b228c91faf748244369ada077c8
Sean T Allen <sean@monkeysnatchbanana.com>
2010-10-25 13:36:24 -0400

    Added script that did simple yaml test in prep for nate cli tool addition.

---
+++ testers/nate/to_yaml.rb
@@ -0,0 +1,11 @@
+require 'rubygems'
+require 'nate'
+
+hash = { 'ol' => [ 'Fred', 'Joe', 'Ed' ],
+  'h1' => 'Hi There',
+  'a' => { 'link' => 'http://www.example.com', Nate::Engine::CONTENT_ATTRIBUTE => 'click here' }  
+}
+
+yaml = hash.to_yaml
+
+puts yaml
